467f83db16d50e71f8ee563e2a055bcc20b6bf37
Tamim Islam <nottamimislam@gmail.com>
2025-05-08 20:50:09 +0600

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
-import AppRoutes from './routes/AppRoutes.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
-import { CartProvider } from './context/CartContext.jsx'
+import AppRoutes from 'routes/AppRoutes.jsx'
+import { AuthProvider } from 'context/AuthContex.jsx'
+import { CartProvider } from 'context/CartContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
